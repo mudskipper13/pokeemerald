e4138ea2a0ca59ad033c29812b7baa726fddff88
mudskipper13 <MariaJumpMan001@hotmail.com>
2023-11-28 17:35:17 +0700

    Code: Outfit system cleanups
    Changes:
    * Put strings into seperate const because agbcc dislikes COMPOUND_STRING
    * Expand desc win of the outfit menu to 27
    * Fix something I didn't check with building

--- src/data/outfit_tables.h
+++ src/data/outfit_tables.h
@@ -2,6 +2,14 @@
 #include "constants/event_objects.h"
 #include "constants/trainers.h"
 
+static const u8 sText_OutfitName_UsualGreen[] = _("USUAL GREEN");
+static const u8 sText_OutfitDesc_UsualGreen[] = _(
+    "The usual, but basic OUTFIT.");
+
+static const u8 sText_OutfitName_UnusualRed[] = _("UNUSUAL RED");
+static const u8 sText_OutfitDesc_UnusualRed[] = _(
+    "Rather unusual, but still basic OUTFIT.");
+
 static const u8 sRegionMapPlayerIcon_BrendanGfx[] = INCBIN_U8("graphics/pokenav/region_map/brendan_icon.4bpp");
 static const u16 sRegionMapPlayerIcon_BrendanPal[] = INCBIN_U16("graphics/pokenav/region_map/brendan_icon.gbapal");
 
@@ -87,10 +95,10 @@ const struct Outfit gOutfits[OUTFIT_COUNT] =
         .prices = { 0, 0 }, //! DESC: prices for purchasing them.
 
         //! DESC: outfit's name
-        .name = COMPOUND_STRING("USUAL GREEN"),
+        .name = sText_OutfitName_UsualGreen,
 
         //! DESC: outfit's description
-        .desc = COMPOUND_STRING("The usual, but basic OUTFIT."),
+        .desc = sText_OutfitDesc_UsualGreen,
 
         //! DESC: palettes for the bg of the mugshot battle transition
         .mugshotPals = { sMugshotPal_Brendan, sMugshotPal_May },
@@ -118,8 +126,8 @@ const struct Outfit gOutfits[OUTFIT_COUNT] =
     [OUTFIT_UNUSUAL_RED] = {
         .isHidden = FALSE,
         .prices = { 200, 500 },
-        .name = COMPOUND_STRING("UNUSUAL RED"),
-        .desc = COMPOUND_STRING("Rather unusual, but still basic OUTFIT."),
+        .name = sText_OutfitName_UnusualRed,
+        .desc = sText_OutfitDesc_UnusualRed,
         .mugshotPals = { sMugshotPal_Brendan, sMugshotPal_May },
         .trainerPics = TRAINER_ID(RUBY_SAPPHIRE_BRENDAN, RUBY_SAPPHIRE_MAY),
         .avatarGfxIds = AVATAR_GFX_ID(BRENDAN, MAY),
