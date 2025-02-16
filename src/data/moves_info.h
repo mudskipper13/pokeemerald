94505a487fc7ac0523cbddac52069d2cb447a233
TeamAquasHideout <jonathonheddings@gmail.com>
2025-02-16 04:53:25 -0500

    Change Anim for Smart Strike

--- src/data/moves_info.h
+++ src/data/moves_info.h
@@ -16111,7 +16111,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_DYNAMAX] =
         .contestCategory = CONTEST_CATEGORY_SMART,
         .contestComboStarterId = 0,
         .contestComboMoves = {0},
-        .battleAnimScript = Move_SMART_STRIKE,
+        .battleAnimScript = Move_PIN_MISSILE,
     },
 
     [MOVE_PURIFY] =
