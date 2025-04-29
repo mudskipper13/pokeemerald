xirosrh/wah-20-anniversary

Coin and Battle Point mart support

src/item.c
@@ -1013,3 +1013,13 @@ u32 GetItemStatus2Mask(u16 itemId)
     else
         return 0;
 }
+
+u32 ItemId_GetCoinPrice(u16 itemId)
+{
+    return gItemsInfo[SanitizeItemId(itemId)].coinPrice;
+}
+
+u32 ItemId_GetBpPrice(u16 itemId)
+{
+    return gItemsInfo[SanitizeItemId(itemId)].bpPrice;
+}

mudskipper13/pokeemerald

Conflicts:
	include/config/general.h
	include/frontier_util.h
	include/item.h
	src/frontier_util.c
	src/new_shop.c
	src/scrcmd.c

src/item.c
@@ -915,3 +915,13 @@ u8 ItemId_GetSecondaryId(u16 itemId)
 {
     return gItems[SanitizeItemId(itemId)].secondaryId;
 }
+
+u32 ItemId_GetCoinPrice(u16 itemId)
+{
+    return gItems[SanitizeItemId(itemId)].coinPrice;
+}
+
+u32 ItemId_GetBpPrice(u16 itemId)
+{
+    return gItems[SanitizeItemId(itemId)].bpPrice;
+}