d79aa94274594cb8d91533e49cc8dba1d419b0ce
mudskipper13 <MariaJumpMan001@hotmail.com>
2025-05-08 20:10:22 +0700

    Fix item description textbox

--- src/overworld.c
+++ src/overworld.c
@@ -3417,7 +3417,7 @@ void ScriptShowItemDescription(struct ScriptContext *ctx)
     PutWindowTilemap(sHeaderBoxWindowId);
     CopyWindowToVram(sHeaderBoxWindowId, 3);
     SetStandardWindowBorderStyle(sHeaderBoxWindowId, FALSE);
-    DrawStdFrameWithCustomTileAndPalette(sHeaderBoxWindowId, FALSE, 0x214, 14);
+    DrawStdFrameWithCustomTileAndPalette(sHeaderBoxWindowId, FALSE, STD_WINDOW_BASE_TILE_NUM, 14);
 
     if (ReformatItemDescription(item, dst) == 1)
         textY = 4;
